65c48703aab2dceb21e785d1e4301edaae7bfaf0
jesseonyangoopiyo <jonyango-opiyo@codedbykids.com>
2022-11-23 17:04:13 -0500

    update2

--- practiceonetest.py
+++ practiceonetest.py
@@ -1,10 +1,71 @@
-from flask import Flask, render_template
+#Code from user @Kjmczk with edits by Jesse
+
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Entry %r>' % self.name
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        name = request.form['name']
+        new_stuff = Entry(name=name)
+
+        try:
+            db.session.add(new_stuff)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problem adding new stuff."
+
+    else:
+        entries = Entry.query.order_by(Entry.created_at).all()
+        return render_template('index.html', entries=entries)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    entry = Entry.query.get_or_404(id)
+
+    try:
+        db.session.delete(Entry)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting data."
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    entry = Entry.query.get_or_404(id)
+
+    if request.method == 'POST':
+        entry.name = request.form['name']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problem updating data."
+
+    else:
+        title = "Update Data"
+        return render_template('update.html', title=title, entry=entry)
 
 
 if __name__ == '__main__':
