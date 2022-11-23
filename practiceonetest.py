25d35bfb0aa00358fb8882c0848fbd6e37a691f2
jesseonyangoopiyo <jonyango-opiyo@codedbykids.com>
2022-11-23 18:17:48 -0500

    new

--- practiceonetest.py
+++ practiceonetest.py
@@ -1,72 +1,64 @@
 #Code from user @Kjmczk with edits by Jesse
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "todo.db"))
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
 
 
-class Entry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Entry %r>' % self.name
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text = db.Column(db.Text)
+    done = db.Column(db.Boolean)
+    dateAdded = db.Column(db.DateTime, default=datetime.now())
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        new_stuff = Entry(name=name)
-
-        try:
-            db.session.add(new_stuff)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was a problem adding new stuff."
-
-    else:
-        entries = Entry.query.order_by(Entry.created_at).all()
-        return render_template('index.html', entries=entries)
+def create_note(text):
+    note = Note(text=text)
+    db.session.add(note)
+    db.session.commit()
+    db.session.refresh(note)
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    entry = Entry.query.get_or_404(id)
-
-    try:
-        db.session.delete(Entry)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return "There was a problem deleting data."
+def read_notes():
+    return db.session.query(Note).all()
 
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    entry = Entry.query.get_or_404(id)
-
-    if request.method == 'POST':
-        entry.name = request.form['name']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was a problem updating data."
-
-    else:
-        title = "Update Data"
-        return render_template('update.html', title=title, entry=entry)
+def update_note(note_id, text, done):
+    db.session.query(Note).filter_by(id=note_id).update({
+        "text": text,
+        "done": True if done == "on" else False
+    })
+    db.session.commit()
 
 
-if __name__ == '__main__':
+def delete_note(note_id):
+    db.session.query(Note).filter_by(id=note_id).delete()
+    db.session.commit()
+
+
+@app.route("/", methods=["POST", "GET"])
+def view_index():
+    if request.method == "POST":
+        create_note(request.form['text'])
+    return render_template("index.html", notes=read_notes())
+
+
+@app.route("/edit/<note_id>", methods=["POST", "GET"])
+def edit_note(note_id):
+    if request.method == "POST":
+        update_note(note_id, text=request.form['text'], done=request.form['done'])
+    elif request.method == "GET":
+        delete_note(note_id)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
